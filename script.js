a0558f166b6d0eddaa42e26735899f8cfe4ddc21
Mayur Kumar <kmayur809@gmail.com>
2021-12-23 21:52:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(window).scroll(function(){
+   var scroll = $(window).scrollTop(),
+   dh = $(document).height(),
+   wh = $(window).height();
+   scrollPercent = (scroll / (dh-wh)) * 100;
+
+   $(".wave").css('top', scrollPercent + "%");
+})
